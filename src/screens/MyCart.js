9870e311c9e25f31ff875a4cca976d3bda6e324d
Rufino Rojas <ruf_rm@hotmail.com>
2022-10-14 13:06:47 -0500

    initial commit and project structure

---
+++ src/screens/MyCart.js
@@ -0,0 +1,10 @@
+export const COLORS ={
+    WHITE: '#FFFFFF',
+    BLACK: '#000000',
+    GREEN: '#00AC76',
+    RED: '#C04345',
+    BLUE: '#0043F9',
+    BACKGROUNDLIGHT: '#F0F0F3',
+    BACKGROUNDMEDIUM: '#B9B9B9',
+    BACKGROUNDDARK: '#777777'
+}
