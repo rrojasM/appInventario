27d0824867d80e962ebd21dfd68f392f13db1cf9
Rufino Rojas <ruf_rm@hotmail.com>
2022-10-16 21:42:06 -0500

    product info

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,174 +1,268 @@
-import React, { useState, useEffect } from 'react';
-import { View, Text, StatusBar, ScrollView, TouchableOpacity, Image } from 'react-native';
-import { COLORS, Items } from '../data/Index';
-import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import React, { useState, useEffect } from "react";
+import {
+  View,
+  Text,
+  StatusBar,
+  ScrollView,
+  TouchableOpacity,
+  Image,
+  StyleSheet
+} from "react-native";
+import { COLORS, Items } from "../data/Index";
+import FontAwesome from "react-native-vector-icons/FontAwesome";
 
 const Home = ({ navigation }) => {
-    const [products, setProducts] = useState([]);
-    const [bebidas, setBebidas] = useState([]);
+  const [products, setProducts] = useState([]);
+  const [bebidas, setBebidas] = useState([]);
 
+  useEffect(() => {
+    const unSubscribe = navigation.addListener("focus", () => {
+      getDataFromData();
+    });
 
-    useEffect(() => {
-        const unSubscribe = navigation.addListener('focus', () => {
-            getDataFromData();
-        });
+    return unSubscribe;
+  }, [navigation]);
 
-        return unSubscribe;
-    }, [navigation]);
+  const getDataFromData = () => {
+    let productList = [];
+    let bebidasList = [];
 
-    const getDataFromData = () => {
-        let productList = [];
-        let bebidasList = [];
-
-        for (let index = 0; index < Items.length; index++) {
-            if (Items[index].category == 'product') {
-                productList.push(Items[index]);
-
-            } else if (Items[index].category == 'bebidas') {
-                bebidasList.push(Items[index]);
-            }
-        }
-
-        setProducts(productList);
-        setBebidas(bebidasList);
+    for (let index = 0; index < Items.length; index++) {
+      if (Items[index].category == "product") {
+        productList.push(Items[index]);
+      } else if (Items[index].category == "bebidas") {
+        bebidasList.push(Items[index]);
+      }
     }
 
+    setProducts(productList);
+    setBebidas(bebidasList);
+  };
 
-    const ProductCard = ({ data }) => {
-        return (
-            <TouchableOpacity
-                onPress={() => navigation.navigate('ProductoInfo', { productID: data.id })}
-                style={{ width: '48%', marginVertical: 14 }}>
-                <View style={{
-                    width: '100%',
-                    height: 100,
-                    borderRadius: 10,
-                    backgroundColor: COLORS.BACKGROUNDLIGHT,
-                    position: 'relative',
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                    marginBottom: 8
-                }}>
-                    {data.isOff ? (
-                        <View
-                            style={{
-                                position: 'absolute',
-                                width: '20%',
-                                height: '24%',
-                                backgroundColor: COLORS.GREEN,
-                                top: 0,
-                                left: 0,
-                                borderTopLeftRadius: 10,
-                                borderBottomRightRadius: 10,
-                                alignItems: 'center',
-                                justifyContent: 'center'
-                            }}
-                        >
-                            <Text style={{ fontSize: 12, color: COLORS.WHITE, fontWeight: 'bold', letterSpacing: 1 }}>{data.offPercentage}%</Text>
-                        </View>
-                    ) : null}
-                    <Image
-                        source={data.productImage}
-                        style={{
-                            width: '80%',
-                            height: '80%',
-                            resizeMode: 'contain'
-                        }}
-                    />
-                </View>
-                <Text style={{ fontSize: 12, color: COLORS.BLACK, fontWeight: '600', marginBottom: 2 }}>
-                    {data.productName}
-                </Text>
-                {data.category === 'bebidas' ? (
-                    data.isAvailable ? (
-                        <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-                            <FontAwesome
-                                name='circle'
-                                style={{
-                                    fontSize: 12,
-                                    marginRight: 6,
-                                    color: COLORS.GREEN
-                                }}
-                            />
-                            <Text style={{ fontSize: 12, color: COLORS.GREEN }}>
-                                Disponible
-                            </Text>
-                        </View>
-                    ) : (
-                        <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-                            <FontAwesome
-                                name='circle'
-                                style={{
-                                    fontSize: 12,
-                                    marginRight: 6,
-                                    color: COLORS.RED
-                                }}
-                            />
-                            <Text style={{ fontSize: 12, color: COLORS.RED }}>
-                                No Disponible
-                            </Text>
-                        </View>
-                    )
-                ) : null}
-                <Text>$ {data.price}</Text>
-            </TouchableOpacity>
-        )
-    }
+  const ProductCard = ({ data }) => {
 
+    console.log('Products Cart', data);
     return (
-        <View style={{ width: '100%', height: '100%', backgroundColor: '#FFF' }}>
-            <StatusBar backgroundColor="#FFF" barStyle="dark-content" />
-            <ScrollView>
-                <View style={{ width: '100%', flexDirection: 'row', justifyContent: 'space-between', padding: 16, backgroundColor: COLORS.GREEN }}>
-                    <TouchableOpacity>
-                        <Image
-                            source={require('../assets/bolso-de-la-tienda.png')}
-                            style={{
-                                width: 35,
-                                height: 35,
-                                fontSize: 12,
-                                marginRight: 6,
-                                color: COLORS.GREEN
-                            }}
-                        />
-                        {/* <Entypo name='shopping-bag' style={{ fontSize: 18, color: COLORS.BACKGROUNDMEDIUM, padding: 12, borderRadius: 10, backgroundColor: COLORS.BACKGROUNDLIGHT }} /> */}
-                    </TouchableOpacity>
-                    <TouchableOpacity onPress={() => navigation.navigate('MyCart')}>
-                        {/*  <MaterialCommunityIcons
-                            name='cart'
-                            style={{ fontSize: 18, color: COLORS.BACKGROUNDMEDIUM, padding: 12, borderRadius: 10, borderWidth: 1, borderColor: COLORS.BACKGROUNDLIGHT }}
-                        /> */}
-                        <Image
-                            source={require('../assets/carrito-de-compras.png')}
-                            style={{
-                                width: 35,
-                                height: 35,
-                                fontSize: 12,
-                                marginRight: 6,
-                                color: COLORS.GREEN
-                            }}
-                        />
-                    </TouchableOpacity>
-                </View>
-                <View style={{ marginBottom: 10, padding: 16, backgroundColor: COLORS.GREEN }}>
-                    <Text style={{ fontSize: 26, color: '#000', fontWeight: '500', letterSpacing: 1, marginBottom: 10 }}>Demo Inventario</Text>
-                    <Text style={{ fontSize: 14, color: '#000', fontWeight: '400', letterSpacing: 1, lineHeight: 24 }}>SubTitulo</Text>
-                </View>
-                <View style={{ padding: 16 }}>
-                    <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between' }}>
-                        <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-                            <Text style={{ fontSize: 18, color: '#000', fontWeight: '500', letterSpacing: 1 }}>Productos</Text>
-                            <Text style={{ fontSize: 14, color: '#000', fontWeight: '400', opacity: 0.5, marginLeft: 10 }}>{products.length}</Text>
-                        </View>
-                        <Text style={{ fontSize: 14, color: COLORS.BLUE, fontWeight: '400' }}>Ver Todos</Text>
-                    </View>
-                    <View style={{ flexDirection: 'row', flexWrap: 'wrap', justifyContent: 'space-around' }}>
-                        {products.map(data => {
-                            return <ProductCard data={data} key={data.id} />
-                        })}
-                    </View>
-                </View>
-                {/* <View style={{ padding: 16 }}>
+      <TouchableOpacity
+        onPress={() =>
+          navigation.navigate("ProductoInfo", { item: data })
+        }
+        style={{ width: "48%", marginVertical: 14 }}
+      >
+        <View
+          style={{
+            width: "100%",
+            height: 100,
+            borderRadius: 10,
+            backgroundColor: COLORS.BACKGROUNDLIGHT,
+            position: "relative",
+            justifyContent: "center",
+            alignItems: "center",
+            marginBottom: 8,
+          }}
+        >
+          {data.isOff ? (
+            <View
+              style={{
+                position: "absolute",
+                width: "20%",
+                height: "24%",
+                backgroundColor: COLORS.GREEN,
+                top: 0,
+                left: 0,
+                borderTopLeftRadius: 10,
+                borderBottomRightRadius: 10,
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+            >
+              <Text
+                style={{
+                  fontSize: 12,
+                  color: COLORS.WHITE,
+                  fontWeight: "bold",
+                  letterSpacing: 1,
+                }}
+              >
+                {data.offPercentage}%
+              </Text>
+            </View>
+          ) : null}
+          <Image
+            source={data.productImage}
+            style={{
+              width: "80%",
+              height: "80%",
+              resizeMode: "contain",
+            }}
+          />
+        </View>
+        <Text
+          style={{
+            fontSize: 12,
+            color: COLORS.BLACK,
+            fontWeight: "600",
+            marginBottom: 2,
+          }}
+        >
+          {data.productName}
+        </Text>
+        {data.category === "bebidas" ? (
+          data.isAvailable ? (
+            <View style={{ flexDirection: "row", alignItems: "center" }}>
+              <FontAwesome
+                name="circle"
+                style={{
+                  fontSize: 12,
+                  marginRight: 6,
+                  color: COLORS.GREEN,
+                }}
+              />
+              <Text style={{ fontSize: 12, color: COLORS.GREEN }}>
+                Disponible
+              </Text>
+            </View>
+          ) : (
+            <View style={{ flexDirection: "row", alignItems: "center" }}>
+              <FontAwesome
+                name="circle"
+                style={{
+                  fontSize: 12,
+                  marginRight: 6,
+                  color: COLORS.RED,
+                }}
+              />
+              <Text style={{ fontSize: 12, color: COLORS.RED }}>
+                No Disponible
+              </Text>
+            </View>
+          )
+        ) : null}
+        <Text>$ {data.price}</Text>
+      </TouchableOpacity>
+    );
+  };
+
+  return (
+    <View style={{ width: "100%", height: "100%", backgroundColor: "#FFF" }}>
+      <StatusBar backgroundColor="#FFF" barStyle="dark-content" />
+      <ScrollView>
+        <View
+          style={{
+            width: "100%",
+            flexDirection: "row",
+            justifyContent: "space-between",
+            padding: 16,
+            backgroundColor: COLORS.GREEN,
+          }}
+        >
+          <TouchableOpacity>
+            <Image
+              source={require("../assets/bolso-de-la-tienda.png")}
+              style={{
+                width: 35,
+                height: 35,
+                fontSize: 12,
+                marginRight: 6,
+                color: COLORS.GREEN,
+              }}
+            />
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => navigation.navigate("MyCart")}>
+            <Image
+              source={require("../assets/carrito-de-compras.png")}
+              style={{
+                width: 35,
+                height: 35,
+                fontSize: 12,
+                marginRight: 6,
+                color: COLORS.GREEN,
+              }}
+            />
+          </TouchableOpacity>
+        </View>
+        <View
+          style={{
+            marginBottom: 10,
+            padding: 16,
+            backgroundColor: COLORS.GREEN,
+          }}
+        >
+          <Text
+            style={{
+              fontSize: 26,
+              color: "#000",
+              fontWeight: "500",
+              letterSpacing: 1,
+              marginBottom: 10,
+            }}
+          >
+            Demo Inventario
+          </Text>
+          <Text
+            style={{
+              fontSize: 14,
+              color: "#000",
+              fontWeight: "400",
+              letterSpacing: 1,
+              lineHeight: 24,
+            }}
+          >
+            Prueba de consepto Inventario
+          </Text>
+        </View>
+        <View style={{ padding: 16 }}>
+          <View
+            style={{
+              flexDirection: "row",
+              alignItems: "center",
+              justifyContent: "space-between",
+            }}
+          >
+            <View style={{ flexDirection: "row", alignItems: "center" }}>
+              <Text
+                style={{
+                  fontSize: 18,
+                  color: "#000",
+                  fontWeight: "500",
+                  letterSpacing: 1,
+                }}
+              >
+                Productos
+              </Text>
+              <Text
+                style={{
+                  fontSize: 14,
+                  color: "#000",
+                  fontWeight: "400",
+                  opacity: 0.5,
+                  marginLeft: 10,
+                }}
+              >
+                {products.length}
+              </Text>
+            </View>
+            <Text
+              style={{ fontSize: 14, color: COLORS.BLUE, fontWeight: "400" }}
+            >
+              Ver Todos
+            </Text>
+          </View>
+          <View
+            style={{
+              flexDirection: "row",
+              flexWrap: "wrap",
+              justifyContent: "space-around",
+            }}
+          >
+            {products.map((data) => {
+              return <ProductCard data={data} key={data.id} />;
+            })}
+          </View>
+        </View>
+
+
+        {/* <View style={{ padding: 16 }}>
                     <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between' }}>
                         <View style={{ flexDirection: 'row', alignItems: 'center' }}>
                             <Text style={{ fontSize: 18, color: '#000', fontWeight: '500', letterSpacing: 1 }}>Bebidas</Text>
@@ -181,9 +275,38 @@ const Home = ({ navigation }) => {
                         </View>
                     </View>
                 </View> */}
-            </ScrollView>
-        </View>
-    )
-}
+      </ScrollView>
+
+      <TouchableOpacity
+        activeOpacity={0.7}
+        style={styles.touchableOpacity}
+      >
+        <Image
+          style={styles.floatImage}
+          source={require('../assets/add.png')}
+        />
+      </TouchableOpacity>
+    </View>
+  );
+};
 
 export default Home;
+
+
+const styles = StyleSheet.create({
+  touchableOpacity: {
+    position: 'absolute',
+    width: 50,
+    height: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+    right: 30,
+    bottom: 30,
+    borderRadius: 10
+  },
+  floatImage: {
+    resizeMode: 'contain',
+    width: 50,
+    height: 50,
+  }
+})
--- src/screens/ProductoInfo.js
+++ src/screens/ProductoInfo.js
@@ -1,12 +1,151 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import React, { useState, useEffect } from 'react';
+import { View, Text, Dimensions, Animated, ToastAndroid, Image, StatusBar, FlatList, TouchableOpacity, ScrollView } from 'react-native';
+import { COLORS, Items } from '../data/Index';
 
-const ProductoInfo = () => {
+const ProductoInfo = ({ route, navigation }) => {
+  const { productID } = route.params;
+  const [product, setProduct] = useState({});
+  const width = Dimensions.get('window').width;
+  const scrollX = new Animated.Value(0);
+  let position = Animated.divide(scrollX, width);
+
+  console.log(product);
+  useEffect(() => {
+    const unSubscribe = navigation.addListener('focus', () => {
+      getDataFromDB();
+    });
+    return unSubscribe;
+  }, [navigation]);
+
+  const getDataFromDB = async () => {
+    for (let index = 0; index < Items.length; index++) {
+      if (Items[index].id == productID) {
+        await setProduct(Items[index]);
+        return
+      }
+    }
+  }
+
+
+  const addToCard = async id => {
+    let itemArray = await AsyncStorage.getItem('cartItems');
+    itemArray = JSON.parse(itemArray);
+
+    if (itemArray) {
+      let array = itemArray;
+      array.push(id);
+
+      try {
+        await AsyncStorage.setItem('cartItems', JSON.stringify(array));
+        ToastAndroid.show(
+          'Producto Agregado Correctamente',
+          ToastAndroid.SHORT,
+        );
+        navigation.navigate('Home');
+      } catch (error) {
+        return error
+      }
+    } else {
+      let array = [];
+      array.push(id);
+      try {
+        await AsyncStorage.setItem('cartItems', JSON.stringify(array));
+        ToastAndroid.show(
+          'Producto Agregado Correctamente',
+          ToastAndroid.SHORT,
+        );
+        navigation.navigate('Home');
+      } catch (error) {
+        return error;
+      }
+    }
+  }
+
+  const renderProduct = ({ item, index }) => {
+    return (
+      <View style={{ width: width, height: 240, alignItems: 'center', justifyContent: 'center' }}>
+        <Image style={{ width: '100%', height: '100%', resizeMode: 'contain' }} />
+      </View>
+    )
+  }
+
   return (
-    <View>
-      <Text>ProductoInfo</Text>
+    <View style={{ width: '100%', height: '100%', backgroundColor: COLORS.WHITE, position: 'relative' }}>
+      <StatusBar backgroundColor={COLORS.BACKGROUNDLIGHT} barStyle="dark-content" />
+      <ScrollView>
+        <View style={{ width: '100%', backgroundColor: COLORS.BACKGROUNDLIGHT, borderBottomRightRadius: 20, borderBottomLeftRadius: 20, position: 'relative', justifyContent: 'center', alignItems: 'center', marginBottom: 4 }}>
+          <View style={{ width: '100%', flexDirection: 'row', justifyContent: 'space-between', paddingTop: 16, paddingLeft: 16 }}>
+            <TouchableOpacity onPress={() => navigation.goBack('Home')}>
+              <Image source={require('../assets/atras.png')} style={{ width: 10, height: 10, padding: 12, borderRadius: 10 }} />
+            </TouchableOpacity>
+          </View>
+
+          <FlatList
+            data={product.productImageList ? product.productImageList : null}
+            horizontal
+            renderItem={renderProduct}
+            showsHorizontalScrollIndicator={false}
+            decelerationRate={0.8}
+            snapToInterval={width}
+            bounces={false}
+            onScroll={Animated.event(
+              [{ nativeEvent: { contentOffset: { x: scrollX } } }],
+              { useNativeDriver: false }
+            )}
+          />
+          <View style={{ width: '100%', flexDirection: 'row', alignItems: 'center', justifyContent: 'center', marginBottom: 16, marginTop: 32 }}>
+            {product.productImageList
+              ? product.productImageList.map((data, index) => {
+                let opacity = position.interpolate({
+                  inputRange: [index - 1, index, index + 1],
+                  outputRange: [0.2, 1, 0.2],
+                  extrapolate: 'clamp',
+                });
+                return (
+                  <Animated.View
+                    key={index}
+                    style={{
+                      width: '16%',
+                      height: 2.4,
+                      backgroundColor: COLORS.BLACK,
+                      opacity,
+                      marginHorizontal: 4,
+                      borderRadius: 100,
+                    }}></Animated.View>
+                );
+              }) : null}
+          </View>
+        </View>
+
+        <View style={{ paddingHorizontal: 16, marginTop: 6 }}>
+          <View style={{ flexDirection: 'row', alignItems: 'center', marginVertical: 14 }}>
+            <Image source={require("../assets/carrito-de-compras.png")} style={{ width: 18, height: 18, marginRight: 6 }} />
+            <Text style={{ fontSize: 12, color: '#000' }}>Carrito de Compras</Text>
+          </View>
+          <View style={{ flexDirection: 'row', marginVertical: 4, alignItems: 'center', justifyContent: 'space-between' }}>
+            <Text style={{ fontSize: 24, fontWeight: '600', letterSpacing: 0.5, marginVertical: 4, color: '#000', maxWidth: '84%' }}>{product.productName}</Text>
+          </View>
+          <Text style={{ fontSize: 12, color: '#000', fontWeight: '400', letterSpacing: 1, opacity: 0.5, lineHeight: 20, maxWidth: '85%', maxHeight: 44, marginBottom: 18 }}>{product.description}</Text>
+          <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between', marginVertical: 14, borderBottomColor: COLORS.BACKGROUNDLIGHT, borderBottomWidth: 1, paddingBottom: 20 }}>
+            <View style={{ flexDirection: 'row', width: '80%', alignItems: 'center' }}>
+              <View style={{ color: COLORS.BLUE, backgroundColor: COLORS.BACKGROUNDLIGHT, alignItems: 'center', justifyContent: 'center', padding: 12, borderRadius: 100, marginRight: 10 }}>
+                <Text style={{ fontSize: 16, color: '#000' }}>Demo</Text>
+              </View>
+            </View>
+          </View>
+          <View style={{ paddingHorizontal: 16, }}>
+            <Text style={{ fontSize: 18, fontWeight: '500', maxWidth: '85%', color: '#000', marginBottom: 4 }}>${product.price}</Text>
+          </View>
+        </View>
+      </ScrollView>
+      <View style={{ position: 'absolute', bottom: 10, height: '8%', width: '100%', justifyContent: 'center', alignItems: 'center' }}>
+        <TouchableOpacity onPress={() => (product.isAvailable? addToCard(product.id): null)} style={{ width: '86%', height: '80%', backgroundColor: COLORS.GREEN, borderRadius: 20, justifyContent: 'center', alignItems: 'center' }}>
+          <Text style={{ fontSize: 12, fontWeight: '500', letterSpacing: 1, color: '#FFF', textTransform: 'uppercase' }}>{product.isAvailable ? "Agregado al Carrito" : "No disponible"}</Text>
+        </TouchableOpacity>
+      </View>
     </View>
-  )
+  );
 }
 
 export default ProductoInfo;
